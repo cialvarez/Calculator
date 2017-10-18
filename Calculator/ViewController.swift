bf6c558998d103b4a1ae5044a1263e6fc0a1a321
christian alvarez <o2jam626@yahoo.com>
2017-10-18 11:06:35 +0800

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Christian Alvarez on 18/10/2017.
+//  Copyright © 2017 Travel Book Philippines. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
